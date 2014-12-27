8bf7869f62547381a47934eca5b6d310c5268e37
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-12-27 13:55:45 +0300

    define vec2 component setf functions

--- lgame.lisp
+++ lgame.lisp
@@ -31,7 +31,9 @@
 (defun vec2 (x y) (vector x y))
 
 (defun vec2-x (v) (aref v 0))
+(defun (setf vec2-x) (val v) (setf (aref v 0) val))
 (defun vec2-y (v) (aref v 1))
+(defun (setf vec2-y) (val v) (setf (aref v 1) val))
 
 
 
