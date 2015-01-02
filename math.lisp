970c13c8362b87a91d8957687100d1d2e265ef3c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-01-03 02:19:22 +0300

    force vec2 element-type to be single-float

--- math.lisp
+++ math.lisp
@@ -4,11 +4,14 @@
 ;;; vec2
 ;;;
 
-(defun vec2 (x y) (vector x y))
+(defun vec2 (x y)
+  (let ((v (make-array 2 :element-type 'single-float)))
+    (setf (aref v 0) (float x) (aref v 1) (float y))
+    v))
 
 (defun vec2-x (v) (aref v 0))
-(defun (setf vec2-x) (val v) (setf (aref v 0) val))
+(defun (setf vec2-x) (val v) (setf (aref v 0) (float val 1.0)))
 (defun vec2-y (v) (aref v 1))
-(defun (setf vec2-y) (val v) (setf (aref v 1) val))
+(defun (setf vec2-y) (val v) (setf (aref v 1) (float val 1.0)))
 
 
