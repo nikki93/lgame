87ca558d9d7831acfbb1b503cd7f3279349d2bbc
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-01-03 06:02:29 +0300

    use macros for vec2, mat3 for efficiency

--- math.lisp
+++ math.lisp
@@ -4,15 +4,15 @@
 ;;; vec2
 ;;;
 
-(defun vec2 (x y)
-  (let ((v (make-array 2 :element-type 'single-float)))
-    (setf (aref v 0) (float x) (aref v 1) (float y))
-    v))
+(defmacro vec2 (x y)
+  `(let ((v (make-array 2 :element-type 'single-float)))
+     (setf (vec2-x v) ,x (vec2-y v) ,y)
+     v))
 
-(defun vec2-x (v) (aref v 0))
-(defun (setf vec2-x) (val v) (setf (aref v 0) (float val 1.0)))
-(defun vec2-y (v) (aref v 1))
-(defun (setf vec2-y) (val v) (setf (aref v 1) (float val 1.0)))
+(defmacro vec2-x (v) `(aref ,v 0))
+(defmacro vec2-y (v) `(aref ,v 1))
+(defsetf vec2-x (v) (val) `(setf (aref ,v 0) (float ,val 1.0)))
+(defsetf vec2-y (v) (val) `(setf (aref ,v 1) (float ,val 1.0)))
 
 
 
@@ -20,17 +20,17 @@
 ;;; mat3
 ;;;
 
-(defun mat3 (m00 m01 m02
+(defmacro mat3 (m00 m01 m02
              m10 m11 m12
              m20 m21 m22)
-  (let ((m (make-array '(3 3) :element-type 'single-float)))
-    (setf (mat3-ref m 0 0) m00 (mat3-ref m 1 0) m10 (mat3-ref m 2 0) m20
-          (mat3-ref m 0 1) m01 (mat3-ref m 1 1) m11 (mat3-ref m 2 1) m21
-          (mat3-ref m 0 2) m02 (mat3-ref m 1 2) m12 (mat3-ref m 2 2) m22)
+  `(let ((m (make-array '(3 3) :element-type 'single-float)))
+    (setf (mat3-ref m 0 0) ,m00 (mat3-ref m 1 0) ,m10 (mat3-ref m 2 0) ,m20
+          (mat3-ref m 0 1) ,m01 (mat3-ref m 1 1) ,m11 (mat3-ref m 2 1) ,m21
+          (mat3-ref m 0 2) ,m02 (mat3-ref m 1 2) ,m12 (mat3-ref m 2 2) ,m22)
     m))
 
-(defun mat3-ref (m i j) (aref m i j))
-(defun (setf mat3-ref) (val m i j) (setf (aref m i j) (float val 1.0)))
+(defmacro mat3-ref (m i j) `(aref ,m ,i ,j))
+(defsetf mat3-ref (m i j) (val) `(setf (aref ,m ,i ,j) (float ,val 1.0)))
 
 (defun transform->mat3 (scale rot trans)
   "Return a matrix representing the given scaling (a vec2), rotation (an angle)
